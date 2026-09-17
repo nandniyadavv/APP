3c70ad8f5b9e02ffec33c5f0b7c2b4eaf683274e
Nandni Yadav <229556217+nandniyadavv@users.noreply.github.com>
2026-09-17 12:45:22 +0530

    Add Week 7 College Management Student class

---
+++ week 7/CollegeManagement/student/Student.java
@@ -0,0 +1,25 @@
+package student;
+
+public class Student {
+    private String name;
+    private int rollNo;
+
+    public Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void display() {
+        System.out.println("Student Details");
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+    }
+}
